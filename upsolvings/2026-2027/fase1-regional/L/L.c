2e61c3e7da04808477a7a3623068666493a57831
Henrique Becker <hbecker@inf.ufrgs.br>
2026-09-23 13:01:17 -0300

    Giving attribution.

--- upsolvings/2026-2027/fase1-regional/L/L.c
+++ upsolvings/2026-2027/fase1-regional/L/L.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Baseado em: https://www.youtube.com/watch?v=NOY6BtziAWs
+// Título: 2026 Sub-Regional Programming Marathon Solution - Problem B
+// Canal: Programando com Gabriel Dalalio
 int main(void) {
     printf("31 35\n");
     printf("*....#...#...#...#...#...#...#...#X\n");for(int i=0;i<7;++i){
